ed987275aec09f9e2f0bf476f9d8676fee061cf8
degawl <devongawley@gmail.com>
2018-10-23 20:35:02 +0300

    Button openURL func

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-    const { title, artist, thumbnail_image, image } = album;
+    const { title, artist, thumbnail_image, image, url } = album;
     const { 
         thumbnailStyle, 
         headerTextStyle, 
@@ -36,7 +36,9 @@ const AlbumDetail = ({ album }) => {
                 />
             </CardSection>
             <CardSection style={cardStyle}>
-                <Button onPress={() => console.log(title)} />
+                <Button onPress={() => Linking.openURL(url)}>
+                    Buy Now
+                </Button>
             </CardSection>
         </Card>
     );
