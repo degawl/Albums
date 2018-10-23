5080670b479033d89617c9d119104a963e5039b2
degawl <devongawley@gmail.com>
2018-10-23 20:07:55 +0300

    Button component setup

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
     const { title, artist, thumbnail_image, image } = album;
-    const { thumbnailStyle, headerTextStyle, headerContentStyle, thumbnailContainerStyle, imageStyle } = styles;
+    const { 
+        thumbnailStyle, 
+        headerTextStyle, 
+        headerContentStyle, 
+        thumbnailContainerStyle, 
+        imageStyle 
+    } = styles;
 
     return (
         <Card>
@@ -26,6 +33,9 @@ const AlbumDetail = ({ album }) => {
                     style={imageStyle}
                     source={{ uri: image }} 
                 />
+            </CardSection>
+            <CardSection>
+                <Button />
             </CardSection>
         </Card>
     );
